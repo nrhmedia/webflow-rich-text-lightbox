c7654b0ba6a89d33c1c0766a5d33828f5ccbccde
nrhmedia <git@nrhmedia.com>
2024-05-14 14:53:08 +0700

    First Commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
-import { greetUser } from '$utils/greet';
+$(document).ready(function () {
+  $('.w-richtext[image-lightbox=true] figure img').each(function () {
+    $(this)
+      .parent()
+      .css({ cursor: 'pointer' })
+      .attr('data-fancybox', 'gallery')
+      .attr('data-src', this.src);
+  });
 
-window.Webflow ||= [];
-window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  $('.w-richtext[image-lightbox=true] img').each(function () {
+    if (!$(this).closest('figure').length) {
+      $(this)
+        .parent()
+        .css({ cursor: 'pointer' })
+        .attr('data-fancybox', 'gallery')
+        .attr('data-src', this.src);
+    }
+  });
+
+  $('[data-fancybox]').fancybox({
+    clickContent: false,
+  });
 });
